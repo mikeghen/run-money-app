ccc49148dbe58039981cc6116f3fdc1da20d8cef
Michael Ghen <mike@mikeghen.com>
2024-06-29 18:10:47 -0400

    change name to run money

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">🏃💸 Proof of Workout</Navbar.Brand>
+        <Navbar.Brand href="/">🏃💸 Run Money</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
